8c5c93dc4385aad9baf00afad1ac025d54be0782
epoche55 <epoche55@gmail.com>
2016-05-01 12:18:25 -0500

    Update parsingAdj.py

--- parsingAdj.py
+++ parsingAdj.py
@@ -1,89 +1,50 @@
 import re
 import pickle
 import string
+import csv
 
-
-def IF(newCode, num):
-	return newCode[num]
-
-def DE(line, lineNum):
-
-	line = line.replace('\t', '')
-	line = line.lstrip(' ') 
-
-	if line[0] == 'R':
-		line = line.replace('R', '')
-		register = int(line.split('=', 1)[0])
-		value = line.split('=', 1)[1]
-		R[register] = value
-	elif (line[0] == 'M') and (line[1] != 'U'):
-		line = line.replace('M', '')
-		register = int(line.split('=', 1)[0])
-		value = line.split('=', 1)[1]
-		M[register] = value
+def splitString(line):
+    # remove punctuation and split line into a list of words
+    line.translate(None, string.punctuation)
+    myLine = line.translate(string.maketrans("",""), string.punctuation)
 	
-	instruction = line.split()
-	print instruction
+    # make all lowercase
+    lineLowerCase = myLine.lower()
 
+    # split string into array
+    wordsInReview = lineLowerCase.split()	
 
+	return wordsInReview
 
+def parseAdj(wordsInReview):
+    # find adjectives within review
+    adjectives = [x for x in allWordsList if x in wordsInReview]
 
+    # add adjectives to list without repeats
+    for element in adjectives:
+        if element not in allAdjList:
+            allAdjList.append(element)
 
+## Open the file with read only permit
+fAllComments = open('AllComments.csv','r')
 
-	# for x in xrange(len(line)):
-	# 	if line[x] == 'R':
-	# 		field1 = line[x:x+1]
-	# 		print field1
+## store all positive words into list
+posWordArr = [posWord.rstrip('\n') for posWord in open('positiveWords.txt')]
+## store all negative words into list
+negWordArr = [negWord.rstrip('\n') for negWord in open('negativeWords.txt')]
+## combine positive and negative lists into one list
+allWordsList = posWordArr + negWordArr
 
-	return instruction
+## create list
+allAdjList = []
 
-def ALU(instex):
-	if (instruction[0] == "ADD"):
-		temp = instex[2] + instex[3] 
-		
-	elif (instruction[0] == "ADDI"):
+## Read the first line
+line = fAllComments.readline()
+while line:
+    wordsInReview = splitString(line)
+    parseAdj(wordsInReview)
+    line = fAllComments.readline()
 
-	return 0
+print allAdjList
 
-def MEM():
-	return 0
-
-
-def WB():
-	return 0
-
-
-code = [line for line in open('test2.txt')]
-
-R = [None]*100
-M = [None]*100
-function = []
-newCode = []
-
-for string in code:
-	rest = string.split('//', 1)[0]
-	newCode.append(rest)
-
-newCode = filter(None, newCode)
-
-lineNum = 0
-for line in newCode:
-	for char in line:
-	    if ':' == char:
-	    	former = line.split(':', 1)[0]
-	    	funcLine = lineNum
-	        rest = line.split(':', 1)[1]
-	        newCode[lineNum] = rest
-	lineNum += 1
-
-# print newCode
-
-number = 0
-for line in newCode:
-	line = IF(newCode, number)
-	inst = DE(line, funcLine)
-	ALU(inst)
-	number += 1
-
-print R
-print M
+fAllComments.close()
